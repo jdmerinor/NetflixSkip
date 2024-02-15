fb4820c6856d7c76d3d701b41b577c4c888d9b76
Juan <juandimr@gmail.com>
2024-02-16 08:02:35 +1000

    update code a bit

--- src/console.js
+++ src/console.js
@@ -1,9 +1,5 @@
 // wrap console.xxx() to add prefix
-const prefix = 'NflxIntroSkip>';
-const console = {
-  log: (...args) => window.console.log(prefix, ...args),
-  warn: (...args) => window.console.warn(prefix, ...args),
-  error: (...args) => window.console.error(prefix, ...args),
-};
+const prefix = 'Netflix Skip > ';
 
-module.exports = console;
+export const log = (...args) => window.console.log(prefix, ...args);
+export const warn = (...args) => window.console.warn(prefix, ...args);
--- src/content.js
+++ src/content.js
@@ -1,8 +1,7 @@
-const console = require('./console');
+import { log, warn } from './console';
 
 (() => {
     window.addEventListener('load', () => {
-        console.log('Version', VERSION);
         bodyObserver.observe(document.body, observerOptions);
     });
 })();
@@ -15,24 +14,25 @@ const observerOptions = {
 const bodyObserver = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
         mutation.addedNodes.forEach(node => {
-                if(node instanceof HTMLElement){
-                    const skipButton = node.querySelector('button[data-uia="player-skip-intro"]');
-                    if(skipButton){
-                        console.log('Skip button seen, clicking..');
-                        // small delay just for visuals
-                        setTimeout(function() {
-                            try{
-                                skipButton.click();
-                            }catch (error) {
-                                console.warn(error);
-                            }}, 800);
-                        try{
-                            skipButton.querySelector('span').innerHTML = "Skipping...";
-                        }catch (error) {
-                            console.warn(error);
+            if (node instanceof HTMLElement) {
+                const skipButton = node.querySelector('button[data-uia="player-skip-intro"]');
+                if (skipButton) {
+                    log('Skip button seen, clicking..');
+                    // small delay just for visuals
+                    setTimeout(function () {
+                        try {
+                            skipButton.click();
+                        } catch (error) {
+                            warn(error);
                         }
+                    }, 800);
+                    try {
+                        skipButton.querySelector('span').innerHTML = "Skipping...";
+                    } catch (error) {
+                        warn(error);
                     }
                 }
+            }
         });
     });
 });
